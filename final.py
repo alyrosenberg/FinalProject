3c395b214cd321ea4181de9eeda87c3132b18915
Aly Rosenberg <alysonpr@umich.edu>
2017-11-21 13:23:53 -0500

    added instructions for each part

--- final.py
+++ final.py
@@ -1,9 +1,32 @@
 #alyson rosenberg
 #final project 206
 
-#going to add part one here
+#PART 1
 
-#part 2 is going here
+#CACHE SETUP HERE
+
+#Utilize the API for a social media site or work tool of your choice 
+#(Facebook, GitHub, Instagram, Gmail, YouTube or any other site you have an active account with)
+#CAN'T BE TWITTER
+
+#Access exactly 100 interactions (posts, emails, commits, likes, etc)
+
+#Find the days that each of these interactions took place (Sunday - Monday)
+
+
+#Write the data to a Database
+
+
+#Create a “report”
+#(screen display, file output, or other easy-to-read format) that shows 
+#how active you are on each day on the site.
+
+
+#If days of the week is not a natural data point,
+#make sure to specify your other metric in the Dec 1st plan.
+
+
+#PART 2
 
 #add visuals here
 
